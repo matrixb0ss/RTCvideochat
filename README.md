19381aef6f59499e6d434bd2ac9a0af1c8f14e5e
matrixb0ss <rinthesoft@gmail.com>
2018-08-28 14:08:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installation
+npm i
+npm start
+
+## Demo
+Demo of the app can be found on Heroku:
+https://webrtc-video-room.herokuapp.com/
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import roomReducer from './room-reducer';
+import audioReducer from './audio-reducer';
+import videoReducer from './video-reducer';
+
+const reducers = combineReducers({
+  rooms: roomReducer,
+  video: videoReducer,
+  audio: audioReducer
+});
+
+export default reducers;
